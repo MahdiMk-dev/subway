e67ea477f5f7d23fc02c093fded8bfd660cc3308
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-04-01 02:45:12 +0300

    Created home page

---
+++ frontend/subway-app/src/pages/displayBranch.jsx
@@ -0,0 +1,12 @@
+import "../styles/utilities.css";
+import "../styles/home.css";
+
+function DisplayBranch() {
+  return (
+    <div className="displayBranch">
+      branch
+    </div>
+  );
+}
+
+export default DisplayBranch;
